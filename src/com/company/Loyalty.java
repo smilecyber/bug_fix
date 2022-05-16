b7fd17b8a2af574f15a78d69d81b0ddef68fcd2b
smilecyber <ismail.yildirim@cybertekschool.com>
2022-05-16 15:46:03 +0200

    Update Loyalty.java

--- src/com/company/Loyalty.java
+++ src/com/company/Loyalty.java
@@ -18,20 +18,34 @@ public class Loyalty {
 
         System.out.println("Please select the deal that you would like attend");
         int dealId = scanner.nextInt();
-
+        if (!(dealId == 1 || dealId == 2 || dealId == 3)){
+            System.out.println("You have entered invalid dealId");
+            return;
+        }
         System.out.println("Please enter item count you would like to buy: ");
         int count = scanner.nextInt();
 
+       if (itemCount < count){
+         System.out.println("not sufficient stock:");
+       }
+        double customerCartTotal = itemPrice * count;
+
+
+        if (dealId == 3){
+            if (customerCartTotal < 30){
+                System.out.println("If the deal 3 is choosen, cart total needs to be greater than 30");
+            }
+        }
+
         int advicedDealId= 0;
         double amountWithSelectedDeal= 0;
-        double customerCartTotal = itemPrice * itemCount;
 
         // deal1 calculations
-        double amountWithDeal1 = customerCartTotal * 0.1;
+        double amountWithDeal1 = customerCartTotal - (customerCartTotal * 0.1);
         // deal2 calculations
         double amountWithDeal2 = customerCartTotal - 5 ;
         // deal3 calculations
-        double amountWithDeal3 = (customerCartTotal * 100)/15;
+        double amountWithDeal3 = (customerCartTotal * 15)/100;
 
 
         if (dealId == 1){
@@ -44,16 +58,19 @@ public class Loyalty {
         double amountWithAdvicedDeal= 0;
 
         if ((amountWithDeal1 < amountWithDeal2)){
-            if ((amountWithDeal2 < amountWithDeal3)){
-                advicedDealId = 3;
+            if ((amountWithDeal1 < amountWithDeal3)){
+                advicedDealId = 1;
                 amountWithAdvicedDeal = amountWithDeal1;
             }else{
-                advicedDealId= 1;
-                amountWithAdvicedDeal = amountWithDeal2;
+                advicedDealId= 3;
+                amountWithAdvicedDeal = amountWithDeal3;
             }
         }else if (amountWithDeal2< amountWithDeal3){
             advicedDealId = 2;
-            amountWithAdvicedDeal = amountWithDeal3;
+            amountWithAdvicedDeal = amountWithDeal2;
+        }else {
+            advicedDealId = 3;
+            amountWithAdvicedDeal = amountWithDeal2;
         }
 
         if (dealId == advicedDealId){
